73a7bcb4367352e4f949281452e4236e587ca8e8
Marta Waplak <mwaplak@gmail.com>
2018-05-11 23:23:14 +0200

    Add project

---
+++ src/CommentsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Comment from './CommentContainer';
+import './CommentsList.css';
+
+const CommentsList = ({comments}) => 
+    <ul>
+      {comments.map(comment => <Comment key={comment.id} {...comment}/>)}
+    </ul>
+    
+export default CommentsList;
