8da90dcf799e010ff106172022db172c0479e77a
Marta Waplak <mwaplak@gmail.com>
2018-05-11 23:32:11 +0200

    Delete all

--- src/CommentsListContainer.js
+++
@@ -1,8 +0,0 @@
-import {connect} from 'react-redux';
-import CommentsList from './CommentsList';
-
-const mapStateToProps = state => ({
-    comments: state.comments
-});
-
-export default connect(mapStateToProps)(CommentsList);
